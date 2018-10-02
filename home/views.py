469c41e6de7b66afdb9e7ff0da37fa050c3409ed
emrahsahan <36627458+emrahsahan@users.noreply.github.com>
2018-10-03 01:57:35 +0300

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from haber.models import Blog
-
+from emlak.models import Emlak
+from proje.models import Proje
 from django.db.models import Count
+
 # Create your views here.
 def home_view(request):
     posts = Blog.objects.all()[:3]
-    return render(request, 'home.html', {'posts': posts})
+    kiralik_list = Emlak.objects.filter(aktif=True, durum='2')[:9]
+    satilik_list = Emlak.objects.filter(aktif=True, durum='1')[:9]
+    proje_list = Proje.objects.all()[:4]
 
+    return render(request, 'home.html', {'posts': posts,
+                                         'kiralik_list': kiralik_list,
+                                         'satilik_list': satilik_list,
+                                         'proje_list': proje_list})
